3e5e35444310d9377b42de2bc05ce10a462e21c2
ntvhoang1711062326 <65747812+ntvhoang1711062326@users.noreply.github.com>
2021-10-13 05:48:12 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
       html: true,
       width: 900,
       padding: "3em",
-      html: "<input type='text' class='form-control' id='txtReason'  placeholder='Whyyy'>",
+      html: "<input type='text' class='form-control' id='txtReason'  placeholder=''>",
       background: '#fff url("img/iput-bg.jpg")',
       backdrop: `
                     rgba(0,0,123,0.4)
